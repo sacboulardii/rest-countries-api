473ce7e38def5750ae5cdbfeba2eefd1b32336c8
canichim <alessandbss@gmail.com>
2022-08-03 00:09:47 -0300

    :recycle: refactor: implement sass syntax

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,24 @@
   <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+nav
+  padding: 30px
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+
+nav a
+  font-weight: bold
+  color: #2c3e50
+
+
+nav a.router-link-exact-active
+  color: #42b983
 </style>
