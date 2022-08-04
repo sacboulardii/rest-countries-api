858f028d7da5add25a7fa05eeae871061175c961
canichim <alessandbss@gmail.com>
2022-08-04 13:56:03 -0300

    :wrench: build: implement style-resources-loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'sass',
+      patterns: [path.resolve(__dirname, './src/sass/*.sass')],
+    },
+  },
 })
