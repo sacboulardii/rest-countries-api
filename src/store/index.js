44c3669a6b910954368d125fc1b749a77dcded46
canichim <alessandbss@gmail.com>
2022-08-04 20:59:39 -0300

    :sparkles: feat: implement Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,37 @@
 import { createStore } from 'vuex'
+import CountriesService from '@/services/CountriesService'
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    countries: null,
+  },
+  mutations: {
+    SET_COUNTRIES(state, countries) {
+      state.countries = countries
+    },
+    SET_FILTERED_COUNTRIES(state, countries) {
+      state.countries = countries
+    },
+  },
+  actions: {
+    fetchCountries({ commit }) {
+      return CountriesService.getCountries()
+        .then((response) => {
+          commit('SET_COUNTRIES', response.data)
+        })
+        .catch((e) => {
+          throw e
+        })
+    },
+    fetchCountryByName({ commit }, name) {
+      return CountriesService.getCountryByName(name)
+        .then((response) => {
+          commit('SET_FILTERED_COUNTRIES', response.data)
+        })
+        .catch((e) => {
+          throw e
+        })
+    },
+  },
   modules: {},
 })
