5b354c562e097cd8432e420b0a03cf3f6db82789
canichim <alessandbss@gmail.com>
2022-08-07 09:31:27 -0300

    :sparkles: feat: create a CountryDetails component

---
+++ src/components/CountryDetails.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="country-details">
+    <div class="country-details__image">
+      <figcaption>
+        <img :src="image" alt="'Teste' + ' Flag'" />
+      </figcaption>
+    </div>
+    <div class="country-details__description">
+      <h2 class="country-details__title">
+        {{ name }}
+      </h2>
+      <div class="details">
+        <div class="details__main">
+          <p class="details__subject">
+            Native Name:
+            <span class="subject-data"></span>
+          </p>
+          <p class="details__subject">
+            Population:
+            <span class="subject-data">{{ population }}</span>
+          </p>
+          <p class="details__subject">
+            Region:
+            <span class="subject-data">{{ region }}</span>
+          </p>
+          <p class="details__subject">
+            Sub Region:
+            <span class="subject-data">{{ subRegion }}</span>
+          </p>
+          <p class="details__subject">
+            Capital:
+            <span class="subject-data">{{ capital }}</span>
+          </p>
+        </div>
+        <div class="details__other">
+          <p class="details__subject">
+            Top Level Domain:
+            <span class="subject-data">{{ topLevelDomain }}</span>
+          </p>
+          <p class="details__subject">
+            Currencies:
+            <span class="subject-data">{{ currencies }} </span>
+          </p>
+          <p class="details__subject">
+            Languages:
+            <span class="subject-data">{{ languages }}</span>
+          </p>
+        </div>
+      </div>
+      <div class="border-countries">
+        <h3 class="border-countries__title">Border Countries:</h3>
+        <ul class="border-countries__list">
+          <li class="border-countries__item"><a href=""></a></li>
+          <li class="border-countries__item"><a href=""></a></li>
+          <li class="border-countries__item"><a href=""></a></li>
+        </ul>
+        <div>{{ borderCountries }}</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CountryDetails',
+  props: {
+    name: String,
+    image: String,
+    nativeName: String,
+    population: Number,
+    region: String,
+    subRegion: String,
+    capital: Array,
+    topLevelDomain: Array,
+    currencies: Object,
+    languages: Object,
+    borderCountries: Array,
+  },
+}
+</script>
+
+<style></style>
