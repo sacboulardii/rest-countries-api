e514a8457806e5ae14a527db03437668ce8edd4c
canichim <alessandbss@gmail.com>
2022-08-07 23:21:34 -0300

    :lipstick: feat: add desktop breakpoint styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <header class="container">
-    <router-link to="/">
-      <h1 class="title">Where in the world?</h1>
-    </router-link>
-    <div class="switch" @click="switchMode">
-      <font-awesome-icon icon="fa-moon" class="icon" />
-      <span class="mode">Dark Mode</span>
+  <header>
+    <div class="inner">
+      <router-link to="/">
+        <h1 class="title">Where in the world?</h1>
+      </router-link>
+      <div class="switch" @click="switchMode">
+        <font-awesome-icon icon="fa-moon" class="icon" />
+        <span class="mode">Dark Mode</span>
+      </div>
     </div>
   </header>
   <router-view />
@@ -43,10 +45,6 @@ export default {
 header {
   background-color: var($--theme-foreground);
 
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
   -webkit-box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.075);
   -moz-box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.075);
   box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.075);
@@ -56,6 +54,16 @@ header {
 
   position: relative;
   z-index: 10;
+
+  & .inner {
+    max-width: 1440px;
+    margin-left: auto;
+    margin-right: auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 }
 
 body {
@@ -65,8 +73,9 @@ body {
 }
 
 .container {
-  padding-left: 5%;
-  padding-right: 5%;
+  max-width: $desktop;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .title {
