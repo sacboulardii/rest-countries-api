aed33690d60e116bddf95e22100ea3b6912f1a21
canichim <alessandbss@gmail.com>
2022-08-08 17:57:52 -0300

    :sparkles: feat(not-found): optional resource param

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,17 @@
 <template>
-  <h2>Oops! The page you're looking for is not here!</h2>
+  <h2>Oops! The {{ resource }} you're looking for is not here!</h2>
   <router-link :to="{ name: 'home' }">Back to the Home Page</router-link>
 </template>
 
 <script>
 export default {
   name: 'NotFound',
+  props: {
+    resource: {
+      type: String,
+      default: 'page',
+    },
+  },
 }
 </script>
 
