0b3f6523998736efdb96f3c1f227094550e4914c
canichim <alessandbss@gmail.com>
2022-08-13 22:21:10 -0300

    :zap: perf: fetch only needed data

--- src/services/CountriesService.js
+++ src/services/CountriesService.js
@@ -9,20 +9,29 @@ const apiClient = axios.create({
   },
 })
 
+/**
+ * Data fields
+ * @desc Passed to API to filter request output and prevent unnecessary data
+ */
+const cardsFields = 'name,flags,population,region,capital'
+const borderCountriesFields = 'name'
+
 export default {
   fetchCountries() {
-    return apiClient.get('/all')
+    return apiClient.get(`/all?fields=${cardsFields}`)
   },
   fetchCountryByName(name) {
     if (name) return apiClient.get(`/name/${name}`)
   },
   fetchCountryByRegion(region) {
     // Return all countries if region filter option is All
-    if (region === 'All') return apiClient.get('/all')
-    return apiClient.get(`/region/${region}`)
+    if (region === 'All') return apiClient.get(`/all?fields=${cardsFields}`)
+    return apiClient.get(`/region/${region}?fields=${cardsFields}`)
   },
   fetchBorderCountries(borders) {
     let bordersCodes = Object.values(borders)
-    return apiClient.get(`/alpha?codes=${bordersCodes.join(',')}`)
+    return apiClient.get(
+      `/alpha?codes=${bordersCodes.join(',')}?fields=${borderCountriesFields}`
+    )
   },
 }
