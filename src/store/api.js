744190e7e42c86df4cc52681407edecd3d44b8f9
canichim <alessandbss@gmail.com>
2022-08-23 12:50:59 -0300

    :memo: docs(api-store): add documentation

--- src/store/api.js
+++ src/store/api.js
@@ -4,10 +4,22 @@ import { ref } from 'vue'
 import ENUM from '@/enums'
 
 export const useApiStore = defineStore('api', () => {
-  /* -------------------- STATE -------------------- */
+  /* ---------------------------------------------------------------- */
+  //                              STATE
+  /* ---------------------------------------------------------------- */
+
+  /**
+   * It initializes api state to INIT (0)
+   */
   const apiState = ref(ENUM.INIT)
 
-  /* -------------------- ACTIONS -------------------- */
+  /* ---------------------------------------------------------------- */
+  //                              ACTIONS
+  /* ---------------------------------------------------------------- */
+
+  /**
+   * It sets the api state to the payload
+   */
   function setApiState(payload) {
     apiState.value = payload
   }
