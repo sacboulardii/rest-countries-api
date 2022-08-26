f36d3f1d94ac83ceb363d5c2dcd58212258d0ce9
canichim <alessandbss@gmail.com>
2022-08-25 21:04:43 -0300

    :memo: docs(helpers): add documentation

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,21 @@
 import { DesignTheme } from '@/types/DesignTheme'
 import { CountryCardFields } from '@/types/CountryFields'
 
+/**
+ * It takes an array of countries, sorts them alphabetically by their common name, and returns the
+ * sorted array.
+ * @returns An array of countries sorted by name.
+ */
 function sortCountries(countries: CountryCardFields[]) {
   return countries.sort((a, b) => a.name.common.localeCompare(b.name.common))
 }
 
+/**
+ * If the current theme is light, return dark, if the current theme is dark, return wolf, otherwise
+ * return light
+ * @param {DesignTheme} currentTheme - DesignTheme - this is the current theme that the user is using.
+ * @returns The next theme in the sequence.
+ */
 function getNextTheme(currentTheme: DesignTheme): DesignTheme {
   return (
     (currentTheme === 'light' && 'dark') ||
