9a41c95f8154e5be76cc18b0ab10ea73c881c45f
canichim <alessandbss@gmail.com>
2022-08-27 17:50:09 -0300

    :heavy_minus_sign: chore: remove font awesome dev dependencies

--- src/main.ts
+++ src/main.ts
@@ -7,34 +7,6 @@ import './assets/reset.css'
 import VueClickAway from 'vue3-click-away'
 import VueScrollTo from 'vue-scrollto'
 
-// import the fontawesome core
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-// import font awesome icon component
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-// import specific icons
-import {
-  // faMoon,
-  // faGlasses,
-  faMagnifyingGlass,
-  // faChevronDown,
-  // faCircleChevronUp,
-  // faLightbulb,
-  // faArrowLeftLong,
-} from '@fortawesome/free-solid-svg-icons'
-
-// add icons to the library
-library.add(
-  // faMoon,
-  // faGlasses,
-  faMagnifyingGlass
-  // faChevronDown
-  // faCircleChevronUp,
-  // faLightbulb,
-  // faArrowLeftLong
-)
-
 // Create pinia store
 const pinia = createPinia()
 
@@ -44,5 +16,4 @@ createApp(App)
   .use(pinia)
   .use(router)
   .use(VueClickAway)
-  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
