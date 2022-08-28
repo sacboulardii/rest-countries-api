175bb092190aad1908225b4f8db9f0096981c198
canichim <alessandbss@gmail.com>
2022-08-28 07:57:30 -0300

    :sparkles: feat: implement BaseButton component variants

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,11 @@
 <template>
-  <button :class="[$style.baseButton, $style[variant]]" v-bind="$attrs">
+  <button
+    :class="[
+      $style.baseButton,
+      ...variant.split(' ').map((variant) => $style[variant]),
+    ]"
+    v-bind="$attrs"
+  >
     <slot />
   </button>
 </template>
@@ -9,6 +15,7 @@ export default {
   props: {
     variant: {
       type: String,
+      default: '--none',
       validators: (value) => ['--sm', '--round'].includes(value),
     },
   },
@@ -18,16 +25,36 @@ export default {
 <style lang="scss" module>
 .baseButton {
   background-color: var($--theme-foreground);
+
   border: none;
   border-radius: 0.15rem;
-  color: var($--theme-font-color);
-  padding: 0.625rem 1.875rem;
+
   box-shadow: 0px 0px 6px 0px hsl(0deg, 0%, 0%, 0.25);
+
+  color: var($--theme-font-color);
+  cursor: pointer;
+
+  font-size: 0.875rem;
+
+  padding: 0.375rem 1.5rem;
+
   text-transform: capitalize;
+
   transition: color 0.4s ease, background-color 0.4s ease;
+
+  @media (min-width: 1024px) {
+    padding: 0.5rem 2rem;
+  }
 }
 
 .--round {
   border-radius: $border-radius-lg;
+}
+
+.--sm {
+  box-shadow: 0px 0px 5px 0px rgba(0, 0%, 0%, 0.2);
+  font-size: 12px;
+
+  padding: 0.375rem 0.5rem;
 }
 </style>
