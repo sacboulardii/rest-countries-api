77471db22d72a75f4fd411c2ab31056d88b76c82
canichim <alessandbss@gmail.com>
2022-08-28 08:14:27 -0300

    :sparkles: feat: implement BorderCountriesListItem component

--- src/components/BorderCountriesList.vue
+++ src/components/BorderCountriesList.vue
@@ -1,21 +1,15 @@
 <template>
   <ul class="border-countries-list">
-    <li
+    <BorderCountriesListItem
       v-for="country in store.getBorderCountriesNames"
+      :country="country"
       :key="country"
-      class="border-countries-list__item"
-    >
-      <router-link :to="{ name: 'detail', params: { countryName: country } }">
-        <BaseButton class="border-countries-list__link" variant="--sm">
-          {{ country }}
-        </BaseButton>
-      </router-link>
-    </li>
+    />
   </ul>
 </template>
 
 <script setup>
-import BaseButton from '@/components/BaseButton.vue'
+import BorderCountriesListItem from '@/components/BorderCountriesListItem.vue'
 import { useCountriesStore } from '@/store/countries'
 const store = useCountriesStore()
 </script>
@@ -32,29 +26,6 @@ const store = useCountriesStore()
 
   @media (min-width: 768px) {
     margin-top: 0;
-  }
-
-  &__item {
-    text-align: center;
-    min-width: 100px;
-  }
-
-  &__link {
-    display: block;
-    width: 100%;
-
-    &:hover,
-    &:focus {
-      transform: translateY(-0.2em);
-      box-shadow: 0 0 0 1em rgba(#fff, 0);
-      animation: pulse 0.4s;
-    }
-  }
-}
-
-@keyframes pulse {
-  0% {
-    box-shadow: 0 0 5px 0 rgba(0, 0%, 0%, 0.05);
   }
 }
 </style>
---
+++ src/components/BorderCountriesListItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <li class="border-countries-list__item">
+    <router-link :to="{ name: 'detail', params: { countryName: country } }">
+      <BaseButton class="border-countries-list__link" variant="--sm">
+        {{ country }}
+      </BaseButton>
+    </router-link>
+  </li>
+</template>
+
+<script setup>
+import BaseButton from '@/components/BaseButton.vue'
+import { defineProps } from 'vue'
+
+defineProps({
+  country: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.border-countries-list {
+  &__item {
+    text-align: center;
+    min-width: 100px;
+  }
+
+  &__link {
+    display: block;
+    width: 100%;
+
+    &:hover,
+    &:focus {
+      transform: translateY(-0.2em);
+      box-shadow: 0 0 0 1em rgba(#fff, 0);
+      animation: pulse 0.4s;
+    }
+  }
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: 0 0 5px 0 rgba(0, 0%, 0%, 0.05);
+  }
+}
+</style>
