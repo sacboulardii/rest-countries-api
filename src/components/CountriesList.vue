f982c071b162d348b16f27f48e40e0250f69aa5a
canichim <alessandbss@gmail.com>
2022-08-31 12:10:46 -0300

    :sparkles: feat: implement results not found message with tip

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -14,7 +14,8 @@
   </transition-group>
   <!-- Mensagem de resultado não encontrado -->
   <p class="not-found" v-if="store.isResourceUnavailable">
-    Nenhum resultado foi encotrado.
+    No results for “{{ store.getLastSearchedText }}”
+    <span class="not-found__tip">Check your search for typos.</span>
   </p>
 </template>
 
@@ -44,6 +45,27 @@ const store = useCountriesStore()
   @media (min-width: 1440px) {
     column-gap: 5.3333rem;
     row-gap: 5.3333rem;
+  }
+}
+
+.not-found {
+  font-weight: bold;
+  margin-top: 3rem;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    margin-top: 4rem;
+  }
+
+  @media (min-width: 1024px) {
+    margin-top: 5rem;
+  }
+
+  &__tip {
+    font-weight: initial;
+    font-size: 12px;
+    margin-top: 1rem;
+    display: block;
   }
 }
 </style>
