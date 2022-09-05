4d1b2be27cd1a5480e847ec96a392be414f6a46a
canichim <alessandbss@gmail.com>
2022-09-05 17:28:11 -0300

    :sparkles: feat: add font import

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,8 @@ onBeforeMount(() => {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap');
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.4s ease;
