b0a5042c98bc518fc097a1f750ca3d149075a42a
canichim <alessandbss@gmail.com>
2022-09-06 12:40:20 -0300

    :sparkles: feat: import fields query from new appConstants file

---
+++ src/constants/appConstants.ts
@@ -0,0 +1,7 @@
+/* Creating query strings that will be used to get the fields that we want from the API. */
+
+export const COUNTRY_CARD_FIELDS_QUERY =
+  '?fields=name,flags,population,region,capital'
+
+export const COUNTRY_DETAILS_FIELDS_QUERY =
+  '?fields=name,flags,population,region,capital,subregion,languages,currencies,tld,borders'
--- src/services/CountriesService.ts
+++ src/services/CountriesService.ts
@@ -1,35 +1,31 @@
 import { AxiosResponse } from 'axios'
 import { CountryCardFields, DetailFields } from '@/types/CountryFields'
 import http from '@/api/api'
+import {
+  COUNTRY_CARD_FIELDS_QUERY,
+  COUNTRY_DETAILS_FIELDS_QUERY,
+} from '@/constants/appConstants'
 
 const responseBody = (response: AxiosResponse) => response.data
-
-/* Creating a query string that will be used to get the fields that we want from the API. */
-const cardsFilterQuery: string =
-  '?fields=' + 'name,flags,population,region,capital'
-
-const detailsFilterQuery: string =
-  '?fields=' +
-  'name,flags,population,region,capital,subregion,languages,currencies,tld,borders'
 
 export default {
   /* Fetching all the countries from the API. */
   fetchAll(): Promise<AxiosResponse<CountryCardFields>> {
-    return http.get('/all' + cardsFilterQuery)
+    return http.get('/all' + COUNTRY_CARD_FIELDS_QUERY)
   },
   /* Fetching the country by name. */
   fetchByName(name: string): Promise<AxiosResponse<CountryCardFields>> {
-    return http.get(`/name/${name}` + cardsFilterQuery)
+    return http.get(`/name/${name}` + COUNTRY_CARD_FIELDS_QUERY)
   },
   /* Fetching the countries by region. */
   fetchByRegion(region: string): Promise<AxiosResponse<CountryCardFields>> {
     if (region === 'All') return http.get('/all')
 
-    return http.get(`/region/${region}` + cardsFilterQuery)
+    return http.get(`/region/${region}` + COUNTRY_CARD_FIELDS_QUERY)
   },
   /* Fetching the details of the country by name. */
   fetchDetails(name: string): Promise<AxiosResponse<DetailFields>> {
-    return http.get(`/name/${name}` + detailsFilterQuery)
+    return http.get(`/name/${name}` + COUNTRY_DETAILS_FIELDS_QUERY)
   },
   /* Fetching the border countries of the country. */
   fetchBorderCountries(borders: string): Promise<AxiosResponse<DetailFields>> {
